9e5c4d22a7f3f1c139f25c72ffa5712c10dab32a
Connor Prentice <connurp@gmail.com>
2024-01-31 20:43:30 -0600

    fixed layout.s because it was throwing an error

--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,26 @@
 import { Stack } from "expo-router";
-import { useCallback } from "react";
 import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
 
-SplashScreen.preventAutoHideAsync();
+export const unstable_settings = {
+  initialRouteName: "home",
+};
 
 const Layout = () => {
-    const [fontsLoaded, fontError] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
-    })
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded || fontError) {
-          await SplashScreen.hideAsync();
-        }
-      }, [fontsLoaded, fontError]);
-    
-      if (!fontsLoaded && !fontError) {
-        return null;
-      }
+  if (!fontsLoaded) {
+    return null;
+  }
 
-    return <Stack onLayout={onLayoutRootView} />
-}
+  return (
+    <Stack initialRouteName="home">
+      <Stack.Screen name="home" />
+    </Stack>
+  )
+};
 
 export default Layout;
